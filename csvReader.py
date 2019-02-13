018fec82995475090183a7dc003c3ba46abf3980
Ciaran McGarry <cmcgarr@tcd.ie>
2019-02-13 13:15:08 +0000

    Update csvReader.py
    Reads csv file and converts to prettified json. csv.Dict is an interesting library that should be investigated further for data manipulation
    
    Needs to be extended to take json and return only values after a certain time and date

--- csvReader.py
+++ csvReader.py
@@ -1,21 +1,30 @@
 import csv
 import json
-import os.path
+import sys
+from pathlib import Path
+
+dirPath = Path.cwd().parents[0] / "Rocksteady" / "Data"
 
 def csvToJSON(filename):
+    # Create filepath
+    filepath = dirPath / filename
     # Open the CSV
-    f = open(filename, 'rU' )
-    # Change each fieldname to the appropriate field name. I know, so difficult.
-    reader = csv.DictReader( f, fieldnames = ( "title", "date", "noArticles", "terms", "positive", "negative",\
+    file = Path.open(filepath, 'r' )
+    # Give appropriate fieldnames to dictionary
+    reader = csv.DictReader( file, fieldnames = ( "title", "date", "noArticles", "terms", "positive", "negative",\
                                                 "active", "passive", "strong", "weak", "Econ@", "Polit", "Milit"))
     # Parse the CSV into JSON
-    out = json.dumps( [ row for row in reader ] )
-    print "JSON parsed!"
+    out = json.dumps( [ row for row in reader ], sort_keys=True, indent=4, )
+    print("JSON parsed!")
+    print(out)
     # Save the JSON
     f = open( filename, 'w')
     f.write(out)
     return out
 
-def main(filename):
-    csvToJSON(filename)
-    print "done"
+def main():
+    csvToJSON(sys.argv[1])
+    print("done")
+
+if __name__ == '__main__':
+  main()
