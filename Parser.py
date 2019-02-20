65378980e507d6a9a81569a85bfe3b6397cbd809
Ciaran McGarry <cmcgarr@tcd.ie>
2019-02-20 13:49:19 +0000

    Update Parser.py
    Added contains, addtofile, compareDateTime, filterJSON functions
    
    Added comments
    
    As yet untested

--- Parser.py
+++ Parser.py
@@ -4,13 +4,13 @@ import sys
 import datetime
 from pathlib import Path
 
-dirPath = Path.cwd().parents[0] / "Rocksteady" / "Data"
+DIR_PATH = Path.cwd().parents[0] / "Rocksteady" / "Data"
 JSON_PATH = "JSON/JSON.json"
 
 # Get JSON data from CSV file
 def csvToJSON(filename):
     #Create filepath to csv
-    filepath = dirPath / filename
+    filepath = DIR_PATH / filename
     # Open the CSV
     file = Path.open(filepath, 'r' )
     # Give appropriate fieldnames to dictionary
@@ -26,29 +26,62 @@ def csvToJSON(filename):
     return out
 
 # Input[0]: datetime object
-# Input[1]: time object indicating tolerance
+# Input[1]: datetime.timedelta object indicating tolerance
 # Output: json of valid sentiment values
 def filterJSON(dateT, tolerance):
-    input_dict = json.loads(JSON_PATH)
+    file = open(JSON_PATH)
+    input_dict = json.loads(file)
     output_dict = [x for x in input_dict if compareDateTime(parseDateTime(x['date']), dateT, tolerance) == 1]
     output_json = json.dumps(output_dict)
     return output_json
 
-# Description: compares two datetime object with a certain tolerance and returns a boolean
+# Description: compares two datetime.datetime object with a certain datetime.timedelta tolerance and returns a boolean
 # Input[0]: datetime object
 # Input[1]: datetime object
 # Input[2]: time object indicating tolerance
 def compareDateTime(dateTime1, dateTime2, tolerance):
+    result = False
+    if (datetime1 - datetime2) <= tolerance:
+        result = True
     return result
 
+# TODO
 def parseHeadlines(data, time, tolerance):
     return
 
-# Add new sentiment values to JSON file taking care not to overwright old ones
-def addToFile():
+# Add new sentiment values to JSON file taking care not to duplicate
+# Input[0]: single sentValue element of form found in JSON doc
+# Input[1]: path of the tile to be loaded/altered
+def addToFile(sentValue, filename):
+    file = open(filename)
+    content = json.loads(file)
+
+    if(not contains(sentValue,file)):
+        content.append(sentValue)
+
+    writeToFile(content, filename)
     return
 
-# JSON date: "year-mo-dyT24:60:60.000Z"
+
+# Check whether a sentiment element is contained in the content of a json doc. Returning True if element is contained in content
+# Input[0]: sentiment value of form found in JSON doc
+# Input[1]: content to be searched through
+# Output: boolean value of True if element is found, and false otherwise
+def contains(sentValue, content):
+    i = 0
+    length = len(content)
+    result = false
+
+    while(i < length && result == false):
+        if(sentValue['date'] == content[i]['date']):
+            result = true
+        i++
+
+    return result
+
+# Description: Parse string of a specific format to a datetime.datetime object
+# Input[0]: Date and time as string of form "YYYY-MM-DDTHH:MM:SS.000Z"
+# Output: datetime.datetime value created from given string
 def parseDateTime(string):
     year = int(string[0:4])
     month = int(string[5:7])
@@ -61,6 +94,9 @@ def parseDateTime(string):
     dateT = datetime.datetime(year, month, day, hour, minute, second)
     return dateT
 
+# Description: writes given content to a given filepath location
+# Input[0]: content to be written
+# Input[0]: location to be written to
 def writeToFile(content, filename):
     file = open(filename, "w")
     file.write(content)
