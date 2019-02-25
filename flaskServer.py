edc2db1f54c83d0f8b0bdf0fbed039839022920e
Ciaran McGarry <cmcgarr@tcd.ie>
2019-02-25 20:08:33 +0000

    Comments/small fixes

--- flaskServer.py
+++ flaskServer.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def index():
     return 'Home'
 
+# Description: Should return headlines of articles published within the given date and time
 @app.route('/headlines/', defaults={'tolerance': None, 'datetime': None})
 @app.route('/headlines/<datetime>', defaults={'tolerance': None})
 @app.route('/headlines/<datetime>/<tolerance>')
@@ -19,14 +20,12 @@ def headlinesDT(datetime, tolerance):
         result = 'Not yet implemented for params: ' + datetime + " " + tolerance
     return result
 
-# Description: Take Variables datetime and tolerance from URL and return JSON of sentiment values within that timeframe
+# Description: Take variables datetime and tolerance from URL and return JSON of sentiment values within that timeframe
 # Input[0]: datetime - of the form YYYY-MM-DDTHH:MM:SS.000Z
 # Input[1]: tolerance - timedelta value of number of minutes away from given datetime a value can be
 # Output[0]: returns JSON of all valid sentiment values
 @app.route('/sentiment/<dateT>/<int:tolerance>')
 def sentiment(dateT, tolerance):
-    result = 'WIP'
-
     tol = datetime.timedelta(minutes = tolerance)
     dateT = Parser.parseDateTime(dateT)
     result = Parser.filterJSON(dateT, tol)
